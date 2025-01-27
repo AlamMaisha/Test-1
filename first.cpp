63a51b04ac31badb3618bb250911b5868bce0e84
maisha alam <maisha11mithi@gmail.com>
2025-01-27 21:48:30 +0600

    fibonacci problem

--- first.cpp
+++ first.cpp
@@ -3,6 +3,29 @@ using namespace std;
 
 int main()
 {
-    for(int i = 1; i <=10; i++)
-    cout << i << " ";
+    int n;
+    cin >> n;
+
+    int a = 0;
+    int b = 1;
+
+    cout << a << " " << b << " ";
+
+    for (int i = 1; i <= n-2; i++)
+    {
+        int x = a + b;
+        cout << x << " ";
+        a = b;
+        b = x;
+    }
+    return 0;    
 }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     for(int i = 1; i <=10; i++)
+//     cout << i << " ";
+// }
